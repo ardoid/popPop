9e7b3a40b579aa42772631f0f35482bb67168726
Ardo Septama <ardoseptama@eduroam-1-126.enst.fr>
2013-03-20 10:17:29 +0100

    Initial Commit

---
+++ PopPop/PPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPAppDelegate.h
+//  PopPop
+//
+//  Created by Ardo Septama on 20.03.13.
+//  Copyright (c) 2013 ardosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopPop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopPop
+//
+//  Created by Ardo Septama on 20.03.13.
+//  Copyright (c) 2013 ardosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}
